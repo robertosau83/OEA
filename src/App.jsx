6e52c695dd082fb7ed16f88bcbc50e4ce0c602cc
robertosau83 <robertosau83@gmail.com>
2025-01-19 17:27:18 +0100

    Initial commit for JS  web app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class="text-3xl text-blue-500">ciao</div>
+  );
+}
+
+export default App;
