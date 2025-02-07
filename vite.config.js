efe87b464922052e29db2daef6e2265c2f72fbbb
robertosau83 <robertosau83@gmail.com>
2025-02-07 10:44:39 +0100

    ok con prova nocache index.html 6

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  // build: {
-  //   target: 'esnext',
-  //   rollupOptions: {
-  //     output: {
-  //       entryFileNames: "assets/[name]-[hash].js",
-  //       chunkFileNames: "assets/[name]-[hash].js",
-  //       assetFileNames: "assets/[name]-[hash].[ext]"
-  //     }
-  //   }
-  // },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]"
+      }
+    }
+  },
 });
